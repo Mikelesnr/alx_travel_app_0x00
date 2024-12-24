697c29203d3c061fedaf6488ab90a7168aa78bb8
Mikelesnr <mwanza.n.m@gmail.com>
2024-12-24 20:28:34 +0200

    db seed tested

--- alx_travel_app/listings/management/commands/seed.py
+++ alx_travel_app/listings/management/commands/seed.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from listings.models import Listing
+from alx_travel_app.listings.models import Listing
 
 class Command(BaseCommand):
     help = 'Seed the database with sample listings'
