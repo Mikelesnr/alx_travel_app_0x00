bedb85f8dc4603c67eda64a7cc058015a68a6bba
Mikelesnr <mwanza.n.m@gmail.com>
2024-12-24 20:55:01 +0200

    serializer update

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -8,10 +8,16 @@ class Listing(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 class Booking(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     user = models.CharField(max_length=255)
     booked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking by {self.user} for {self.listing.title}"
 
 class Review(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
@@ -19,3 +25,7 @@ class Review(models.Model):
     rating = models.IntegerField()
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.user} for {self.listing.title}"
+
